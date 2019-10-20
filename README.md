98d521c2c7b746b83ddcd50da816266f82013f07
Saurabh Nair <sbhn4evr@protonmail.com>
2019-10-20 14:48:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Landing Page Project
+## This is a custom page I've created with content that may not be completely accurate, but made for test purpose
+
+* Index page contains 3 main parts:
+    * Header
+    * Content
+    * Footer
+* Header and Footer are made to have fixed position, at the top and bottom respectively
+* The content is displayed between them in a scrollable window.
+* Content consists of **6 different sections**.
+* The header has a navbar dynamically populated on load, with links to different sections of the page.
+* On clicking the link the section is scrolled into view.
+* On scrolling through the page, the current active section has a different background. The navbar link associated with this section also has a different background.
+* The navbar is set to disappear 2 seconds after a scroll event has been fired. It still needs improvement to overwrite the timer after the second scroll has been performed within 2 seconds.
---
+++ js/app.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function(event){
+    /* On content loaded setup the navbar, this step is till line 13*/
+    let sections = document.querySelectorAll('.section>h2');
+    let navbar = document.querySelector('.navbar');
+    navbar.setAttribute('style', 'background-color: darkkhaki;');
+    navbar.innerHTML = '';
+    let count = 0;
+    for(section of sections){
+        navbar.innerHTML += '<li><a href="#'+section.innerHTML+'">'+ section.innerHTML + '</a></li>';
+        section.setAttribute('id', section.innerHTML);
+    }
+    document.querySelector('.navbar>li').setAttribute('style', 'background-color: blanchedalmond;');
+    inView();
+
+    /*Once navbar is set, on scroll of content element set visibility of navbar to visible and after 2 seconds make it invisible(hidden)*/
+    document.querySelector('.content').addEventListener('scroll', function(event){
+        let target = event.target;
+        
+        let nav = document.querySelector('.navbar');
+        let navChildren = document.querySelector('.navbar').querySelectorAll('*');
+        nav.setAttribute('style', 'visibility: visible;');
+        for(navChild of navChildren){
+            navChild.setAttribute('style', 'visibility: visible;');
+        }
+        setTimeout(function makeInvisible(){
+            nav.setAttribute('style', 'visibility: hidden;');
+            for(navChild of navChildren){
+                navChild.setAttribute('style', 'visibility: hidden;');
+            }
+        }, 2000);
+        navbar.setAttribute('style', 'background-color: darkkhaki;');
+        inView();
+    });
+});
+
+/*On clicking navbar link, scroll the desired section into view*/
+document.addEventListener('click', function(event){
+    if(event.target.nodeName === 'A' && event.target.innerHTML != 'click here'){
+        event.target.addEventListener('click', function(e){
+            e.preventDefault();
+            e.target.onclick = document.getElementById(e.target.innerHTML).scrollIntoView();
+            
+            inView();
+        });
+    }
+}, true);
+
+/*This function checks if a content header is below the top of screen and above 50% of viewport height. If so, sets the section as active &additionally changes the background color of corresponding navbar link*/
+function inView(){
+    let sections = document.querySelectorAll('.section');
+    for(section of sections){
+        let locationOfSection = section.getBoundingClientRect();
+        if(locationOfSection.top >=0 && locationOfSection.top < (window.innerHeight || document.documentElement.clientHeight)/2 ){
+            section.setAttribute('style', 'background-color: grey;');
+            let idForNav = section.firstElementChild.innerHTML;
+            let navs = document.querySelectorAll('.navbar a');
+            for(nav of navs){
+                if(nav.innerHTML == idForNav){
+                    nav.setAttribute('style', 'background-color: blanchedalmond;');
+                } else {
+                    nav.setAttribute('style', 'background-color: darkkhaki;');
+                }
+            }
+        } else{
+            section.setAttribute('style', 'background-color: whitesmoke;');
+        }
+    }
+}
